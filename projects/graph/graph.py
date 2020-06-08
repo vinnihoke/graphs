7d2d83093585bcf321e44371cbdaa5a23e91a1ec
Vinni Hoke <vinni.hoke@gmail.com>
2020-06-08 12:24:29 -0700

    Traversals completed

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -13,33 +13,82 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        # Initiate a blank set
+        self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        if v1 in self.vertices and v2 in self.vertices:
+            # Add v2 as a neighbor to v1
+            self.vertices[v1].add(v2)
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        return self.vertices[vertex_id]
 
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        # Create an empty queue and enqueue the starting vertex id
+        q = Queue()
+        q.enqueue(starting_vertex)
+
+        # Create a set to store visited vertices
+        visited = set()
+
+        # While the queue is not empty
+        while q.size() > 0:
+
+            # Dequeue the first vertex
+            v = q.dequeue()
+
+            # If that vertex has not been visited
+            if v not in visited:
+
+                # Visit it
+                print(v)
+
+                # Mark as visited
+                visited.add(v)
+
+                for next_vertex in self.get_neighbors(v):
+                    q.enqueue(next_vertex)
 
     def dft(self, starting_vertex):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        # Create an empty stack and push the starting vertex id
+        s = Stack()
+        s.push(starting_vertex)
+
+        # Create a set to store visited vertices
+        visited = set()
+
+        # While the stack is not empty
+        while s.size() > 0:
+
+            # Pop the last vertex
+            v = s.pop()
+
+            # If that vertex has not been visited
+            if v not in visited:
+
+                # Visit it
+                print(v)
+
+                # Mark it as visited
+                visited.add(v)
+
+                for next_vert in self.get_neighbors(v):
+                    s.push(next_vert)
 
     def dft_recursive(self, starting_vertex):
         """
