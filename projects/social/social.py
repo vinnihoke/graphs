f6764efd2d7a4754332b76a58797e515b557c5fc
Vinni Hoke <vinni.hoke@gmail.com>
2020-06-11 09:46:28 -0700

    Social completed

--- projects/social/social.py
+++ projects/social/social.py
@@ -16,6 +16,8 @@ class Queue():
 class User:
     def __init__(self, name):
         self.name = name
+    def __repr__(self):
+        return self.name
 
 
 class SocialGraph:
@@ -29,12 +31,17 @@ class SocialGraph:
         Creates a bi-directional friendship
         """
         if user_id == friend_id:
-            print("WARNING: You cannot be friends with yourself")
+            # Refactor
+            # print("WARNING: You cannot be friends with yourself")
+            return False
         elif friend_id in self.friendships[user_id] or user_id in self.friendships[friend_id]:
-            print("WARNING: Friendship already exists")
+            # Refactor
+            # print("WARNING: Friendship already exists")
+            return False
         else:
             self.friendships[user_id].add(friend_id)
             self.friendships[friend_id].add(user_id)
+            return True
 
     def add_user(self, name):
         """
@@ -63,24 +70,50 @@ class SocialGraph:
         for i in range(0, num_users):
             self.add_user(f"User {i}")
 
+        # New friendship method:
+        # Randomly generate friendships, keeping new and rejecting duplicates, until we get to the number we need ( num_users * average friendships // 2)
+
+        # Keep track of good friendships and collisions. Refactor add_friendship
+        
+        target_friendships = num_users * avg_friendships // 2 # You can either divide this by two or...:87
+        total_friendships = 0
+        collisions = 0
+
+        while total_friendships < target_friendships:
+            user_id = random.randint(1, self.last_id)
+            friend_id = random.randint(1, self.last_id)
+
+            if self.add_friendship(user_id, friend_id):
+                total_friendships += 1 # Add two here and not divide by two
+            else:
+                collisions += 1
+
+        print(f"Total collisions: {collisions}")
+
+
+
+
+
+
+
         # Create Frienships
         # Generate all possible friendship combinations
-        possible_friendships = []
+        # possible_friendships = []
 
         # Avoid duplicates by ensuring the first number is smaller than the second
-        for user_id in self.users:
-            for friend_id in range(user_id + 1, self.last_id + 1):
-                possible_friendships.append((user_id, friend_id))
+        # for user_id in self.users:
+        #     for friend_id in range(user_id + 1, self.last_id + 1):
+        #         possible_friendships.append((user_id, friend_id))
 
         # Shuffle the possible friendships
-        random.shuffle(possible_friendships)
+        # random.shuffle(possible_friendships)
 
         # Create friendships for the first X pairs of the list
         # X is determined by the formula: num_users * avg_friendships // 2
         # Need to divide by 2 since each add_friendship() creates 2 friendships
-        for i in range(num_users * avg_friendships // 2):
-            friendship = possible_friendships[i]
-            self.add_friendship(friendship[0], friendship[1])
+        # for i in range(num_users * avg_friendships // 2):
+        #     friendship = possible_friendships[i]
+        #     self.add_friendship(friendship[0], friendship[1])
 
     def get_all_social_paths(self, user_id):
         """
@@ -138,9 +171,14 @@ class SocialGraph:
 
 if __name__ == '__main__':
     sg = SocialGraph()
-    sg.populate_graph(10, 2)
-    print("Friendships")
-    print(sg.friendships)
+    sg.populate_graph(10000, 4)
+    # print("Friendships")
+    # print(sg.friendships)
     connections = sg.get_all_social_paths(1)
-    print("Connections")
-    print(connections)
+    # print("Connections")
+    # print(connections)
+
+    total_social_paths = 0
+    for user_id in connections:
+        total_social_paths += len(connections[user_id])
+    print(f"Average length: { total_social_paths / len(connections) }")
